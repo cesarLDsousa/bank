36da8ec6176710ada422342df0065dc0f51e4540
cesarLDsousa <cesarluiz.git@gmail.com>
2024-05-22 23:12:08 -0300

    add account class

---
+++ src/Account.java
@@ -0,0 +1,34 @@
+public abstract class Account {
+    private int number;
+    private double balance;
+
+    public Account(int number, double balance) {
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void debitAmount(double value) {
+        this.balance -= value;
+    }
+
+    public void creditAmount(double value) {
+        this.balance += value;
+    }
+
+}
