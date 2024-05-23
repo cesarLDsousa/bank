3ac05fafeeeb963fd17d32d03e15550bc242621e
cesarLDsousa <cesarluiz.git@gmail.com>
2024-05-22 23:14:55 -0300

    add branch and customer

---
+++ src/Branch.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Branch {
+    private int code;
+    private String city;
+    private final ArrayList<Account> accounts;
+
+    public Branch(int branchCode, String city) {
+        this.code = branchCode;
+        this.city = city;
+        this.accounts = new ArrayList<>();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public void removeAccount(Account account) {
+        accounts.remove(account);
+    }
+
+    public Account getAccount(int number) {
+        return accounts.stream()
+                .filter(account -> account.getNumber() == number)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Account> getAccountsSortedByNumber() {
+        return accounts.stream()
+                .sorted((acc1, acc2) -> Integer.compare(acc1.getNumber(), acc2.getNumber()))
+                .toList();
+    }
+}
