2025a766294ed4983a69e0d932cd603cd0007ca6
cesarLDsousa <cesarluiz.git@gmail.com>
2024-05-22 23:13:02 -0300

    add current account

---
+++ src/CurrentAccount.java
@@ -0,0 +1,39 @@
+public class CurrentAccount extends Account {
+    private double interestRate;
+    private String dateOfOpening;
+    private Customer customer;
+
+    public CurrentAccount(int number, double balance, double interestRate, String dateOfOpening) {
+        super(number, balance);
+        this.interestRate = interestRate;
+        this.dateOfOpening = dateOfOpening;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public String getDateOfOpening() {
+        return dateOfOpening;
+    }
+
+    public void setDateOfOpening(String dateOfOpening) {
+        this.dateOfOpening = dateOfOpening;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void removeCustomer() {
+        this.customer = null;
+    }
+}
